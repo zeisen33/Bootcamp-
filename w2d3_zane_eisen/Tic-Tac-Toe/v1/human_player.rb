f3692ea0f374dd802740fb7da63e6ef4d2b342ce
Zane <zeisen33@gmail.com>
2022-11-16 18:43:29 -0500

    Finish Tic-Tact-Toe v1 board

--- w2d3_zane_eisen/Tic-Tac-Toe/v1/human_player.rb
+++ w2d3_zane_eisen/Tic-Tac-Toe/v1/human_player.rb
@@ -6,30 +6,34 @@ class HumanPlayer
     end
 
     def get_position
-        begin
+        # begin
             p "#{mark_value} Player, enter a position. Type two numbers separated by a space, the first representing the row, and the second representing the column."
             input = gets.chomp
-            if input.split(' ').length > 2
-                rescue 
-                    raise StandardError.new "position input uses invalid format"
-                retry
-        
-            end
-            row = input.split(' ')[0].to_i
-            col = input.split(' ')[1].to_i
+        #     if input.split(' ').length > 2
+        #         raise StandardError.new "position input uses invalid format"
+        #     end
+        #     row = input.split(' ')[0]
+        #     col = input.split(' ')[1]
+        #     row.each_char do |char|
+        #         if !char.instance_of?(String)
+        #             raise StandardError.new p "row and column must be numbers"
+        #         end
+        #     end
+        #     col.each_char do |char|
+        #         if !char.instance_of(String)
+        #             raise StandardError.new p "row and column must be numbers"
+        #         end
+        #     end
+
         # rescue
-        #     raise StandardError.new "row and column must be numbers"
         #     retry
-         end
+        row = input.split(' ')[0]
+        col = input.split(' ')[1]
 
         [row, col]
     end
-
-
-
-        
-
-
-
-
 end
+
+p1 = HumanPlayer.new(":)")
+
+p p1.get_position
