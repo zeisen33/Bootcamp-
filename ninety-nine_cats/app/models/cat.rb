146e99ddababf4ddc3875703523de7af77537a76
Zane <zeisen33@gmail.com>
2022-12-18 18:36:50 -0500

    ninety-nine cats

---
+++ ninety-nine_cats/app/models/cat.rb
@@ -0,0 +1,22 @@
+require "date"
+
+class Cat < ApplicationRecord
+  @@CAT_COLORS = %i[orange tuxedo black gray white calico]
+
+  def self.get_cat_colors
+    @@CAT_COLORS
+  end
+
+  validates :birth_date, :color, :name, :sex, presence: :true
+  validates :color, inclusion: { in: Cat.get_cat_colors }
+  validates :sex, inclusion: { in: %w[M F] }
+
+#   def birth_date_cannot_be_future(birth_date)
+#     #return true if a past date, false if future date
+#     return false if !birth_date.valid?
+#     #date > date.now 
+#     birth_date < Date.today
+#   end
+
+#   validates :birth_date_cannot_be_future
+end
--- w6d2/active_record/lib/01_sql_object.rb
+++ w6d2/active_record/lib/01_sql_object.rb
@@ -4,19 +4,56 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+  require 'active_support/inflector'
   def self.columns
-    # ...
+    DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+      SQL
   end
 
+  def self.columns_helper(cols)
+    sym_cols = []
+    cols.each do |col|
+      sym_cols << col.to_sym
+    end
+    sym_cols
+  end
+
+  def self.columns
+    results = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+      SQL
+    self.columns_helper(results[0])
+  end
+
+  # def self.columns_helper(cols)
+  #   sym_cols = []
+  #   cols.each do |col|
+  #     sym_cols << col.to_sym
+  #   end
+  #   sym_cols
+  # end
+
   def self.finalize!
+    # send attr_accessor to self with each instance variable as args
   end
 
   def self.table_name=(table_name)
-    # ...
+    @@class_name = table_name
   end
 
   def self.table_name
-    # ...
+    @@class_name ||= self.to_s.tableize
   end
 
   def self.all
@@ -55,3 +92,8 @@ class SQLObject
     # ...
   end
 end
+
+class BigDog < SQLObject
+end
+
+# p BigDog.table_name # => "big_dogs"
