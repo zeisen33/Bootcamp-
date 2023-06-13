87b5306fc1b647f4c436d744064d22ff1898a42d
Zane <zeisen33@gmail.com>
2023-06-12 22:20:05 -0400

    Start Flappy

--- Flappy Bird/src/game.js
+++ Flappy Bird/src/game.js
@@ -1,6 +1,17 @@
+import Level from './level'
+
 export default class FlappyBird {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+  }
+
+  restart() {
+    const level = new Level(this.dimensions)
+    this.animate(this.ctx)
+  }
+
+  animate() {
+    Level.animate()
   }
 }
