e55f1b38efc984a4986b4991c21972048d054261
Zane <zeisen33@gmail.com>
2023-06-23 22:57:22 -0400

    Remove changes

--- Flappy Bird/src/game.js
+++ Flappy Bird/src/game.js
@@ -1,17 +1,17 @@
-import Level from './level';
+import Level from './level'
 
 export default class FlappyBird {
   constructor(canvas) {
-    this.ctx = canvas.getContext("2d");
-    this.dimensions = { width: canvas.width, height: canvas.height };
+    this.ctx = canvas.getContext("2d")
+    this.dimensions = { width: canvas.width, height: canvas.height }
   }
 
   restart() {
-    const level = new Level(this.dimensions);
-    this.animate(this.ctx);
+    const level = new Level(this.dimensions)
+    this.animate(this.ctx)
   }
 
   animate() {
-     Level.animate();
+     Level.animate()
   }
 }
