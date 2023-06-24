3415b5fab6017481e471baa4c9852a1619a39107
Zane <zeisen33@gmail.com>
2023-06-23 22:32:44 -0400

    animate

--- Flappy Bird/src/game.js
+++ Flappy Bird/src/game.js
@@ -12,6 +12,6 @@ export default class FlappyBird {
   }
 
   animate() {
-     Level.animate()
+     Level.animate();
   }
 }
--- Flappy Bird/src/level.js
+++ Flappy Bird/src/level.js
@@ -1,10 +1,10 @@
 export default class Level {
   constructor(dimensions) {
-    this.dimensions = dimensions
+    this.dimensions = dimensions;
   }
 
   drawBackground(ctx) {
     ctx.fillStyle = "skyblue";
-    ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height)
+    ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);
   }
 }
