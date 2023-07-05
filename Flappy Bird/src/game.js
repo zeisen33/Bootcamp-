5ad9c4d7667a9da457ca10462554ba4ec4875af2
zeisen33 <zeisen33@gmail.com>
2023-07-05 19:49:57 -0400

    dimensions

--- Flappy Bird/src/game.js
+++ Flappy Bird/src/game.js
@@ -3,6 +3,7 @@ import Level from './level'
 export default class FlappyBird {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d")
+    
     this.dimensions = { width: canvas.width, height: canvas.height }
   }
 
--- Flappy Bird/src/level.js
+++ Flappy Bird/src/level.js
@@ -1,6 +1,7 @@
 export default class Level {
   constructor(dimensions) {
     this.dimensions = dimensions
+    
   }
 
   
