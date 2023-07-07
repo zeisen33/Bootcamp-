d11b44520573fb6089430eceee7e456d43ddb79e
zeisen33 <zeisen33@gmail.com>
2023-07-07 02:47:36 -0400

    Game pt 2

--- Flappy Bird/src/game.js
+++ Flappy Bird/src/game.js
@@ -1,18 +1,21 @@
 import Level from './level'
+import Bird from './bird'
 
 export default class FlappyBird {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d")
-    
     this.dimensions = { width: canvas.width, height: canvas.height }
+    this.restart()
   }
 
   restart() {
+    this.bird = new Bird(this.dimensions)
     this.level = new Level(this.dimensions)
     this.animate()
   }
 
   animate() {
     this.level.animate(this.ctx)
+    this.bird.animate(this.ctx)
   }
 }
