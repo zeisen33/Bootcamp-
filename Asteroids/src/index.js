4c981e5bcaeada18e2a76a84fb8385e91d03fe8f
Zane <zeisen33@gmail.com>
2023-08-11 15:00:43 -0400

    Context setup

--- Asteroids/src/index.js
+++ Asteroids/src/index.js
@@ -1,2 +1,12 @@
+import MovingObject from "./moving_object"
 
-console.log('webpack working')
+window.MovingObject = MovingObject
+
+// console.log('webpack working')
+
+const canvas = document.getElementById('game-canvas')
+const ctx = canvas.getContext('2d')
+
+
+const obj1 = new MovingObject({pos: {x: 50, y: 60}, color: 'green', radius: 10} )
+obj1.draw(ctx)
---
+++ Asteroids/src/moving_object.js
@@ -0,0 +1,26 @@
+
+
+export default class MovingObject {
+    constructor(options) {
+        this.pos = options.pos
+        this.vel = options.vel
+        this.radius = options.radius
+        this.color = options.color
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+
+        ctx.arc(
+            this.pos.x,
+            this.pos.y,
+            this.radius,
+            0,
+            2 * Math.PI,
+            false
+        )
+
+        ctx.fill()
+    }
+}
